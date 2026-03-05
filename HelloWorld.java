109fc06913c7b0c9b58aaf02667c0181870e09fb
yiink-vcu <yiink@vcu.edu>
2026-03-05 11:32:26 -0500

    The first commit in branch 'User1'

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.printf("Hello World!");
+
+        System.out.printf("Locality: Richmond, VA");
     }
 }
